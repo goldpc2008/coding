2f698a863ea07f4c4333444cc5ee7f1c149d7ddf
Goldpc <goldpc2009@qq.com>
2019-10-19 22:32:43 +0800

    Add geoPrint.py

--- geoliteip/geoPrint.py
+++ geoliteip/geoPrint.py
@@ -2,19 +2,36 @@
 # -*- coding:utf-8 -*-
 
 import argparse
+import dpkt
+import socket
 import geoip2.database
 import os
-import re
+
+def printPcap( pcap):
+    for ( ts , buf ) in pcap:
+        try:
+            eth = dpkt.ethernet.Ethernet( buf )
+            ip = eth.data
+            src = socket.inet_ntoa( ip.src )
+            dst = socket.inet_ntoa( ip.dst )
+            print('[*]Src:{0}--->{1}'.format( src , dst))
+            srcInfo = getIPinformation( src )
+            dstInfo = getIPinformation( dst )
+            print('[+]Src Country:{0},City:{1}--->Dst:Country:{2},City:{3}\n'.format(srcInfo['country'],srcInfo['city'],dstInfo['country'],dstInfo['city'] ))
+
+        except:
+            pass
 
 def getIPinformation(  ipAddress ):
     try:
+        #判断GEOIP数据库是否存在当前目录下
         mmdbPath = os.path.join(os.getcwd(),'GeoLite2-City.mmdb')
         if os.path.isfile( mmdbPath) :
             pass
         else:
             print( "[*]The Path: {0} don't found GeoIp Database File.".format( mmdbPath))
             exit(0)
-        
+        rInfo = { }    
         reader  = geoip2.database.Reader( mmdbPath )
         response = reader.city( ipAddress )
         #获得IP地址相关的国家、省、市、邮编、经纬度
@@ -22,68 +39,55 @@ def getIPinformation(  ipAddress ):
             continent = response.continent.names[ 'zh-CN']
         except Exception :
             continent = response.continent.name
+        rInfo['continet'] = continent
         try:
             country = response.country.names['zh-CN']
         except Exception:
             country = response.country.name
+        rInfo['country'] = country
         try:
             province = response.subdivisions.most_specific.names['zh-CN']
         except Exception :
             province = response.subdivisions.most_specific.name
+        rInfo['province'] = province
         try:
             city =  response.city.names[ 'zh-CN']
         except Exception:
             city = response.city.name
+        rInfo['city'] = city
         code = response.postal.code
+        rInfo['code'] = code
         latitude = response.location.latitude
+        rInfo['latitude'] = latitude
         longitude = response.location.longitude
+        rInfo['longitude'] = longitude
         print('\n[*]Target: {0} GeoLiteDB-located:'.format(ipAddress))
         print('[+]Continent:{0}, Country:{1}, Province:{2}, City: {3}, Postcode:{4}'.format( continent,country,province,city,code))
         print('[+]Latitude: {}'.format( latitude ))
         print('[+]Longitude:{}'.format( longitude ))
         #关闭GeoLiteIp数据库
         reader.close()
+        return rInfo
     except Exception as e :
-        print("The Porgrame runing error: {}".format( e ))
+        print("The IP Informat get Error: {}".format( e ))
+        exit(0)
 
-def checkIp( ip ):
-    ll = ip.split( "." )
-    for i in ll :
-        si = int( i )
-        if si<=0 or si>255 :
-            return False
-    return True
+
 
 def main():
-    # Main Program
-    # getIPinformation( '58.247.88.22')
-    # getIPinformation( '220.248.92.59')
-    parser = argparse.ArgumentParser(description="Search IP Address Location City",usage='%(prog)s -I <target IP Address > ')
-    parser.add_argument('-I', dest='tgIp', type=str, help='Ip Address Location City')
-    
-    args = parser.parse_args()
-
-    tgIp = args.tgIp
-    
-    if ( tgIp == None ) :
-        print( "usage: getIPinformation.py -I <target IP Address >  \n\t -I IP Address \n")
-        exit( 0 )
+    #path = r'F:\pythoncode\coding\geoip'
+    path = os.getcwd()
+    pcapFile = os.path.join( path, 'geotest.pcap')
+    if os.path.isfile ( pcapFile ) :
+        with open( pcapFile, 'rb' ) as f :
+            pcap = dpkt.pcap.Reader( f )
+            printPcap( pcap )
     else:
-        p =  re.compile( r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
-        tl = p.findall( tgIp )
-        if tl == None :
-            print( "Not Found IP Address!!! ")
-        else:
-            llen = len( tl )
-            for n in range( llen) :
-                if checkIp( tl[n] ) :
-                    getIPinformation( tl[n] )
-                    # print( tl[n] )
-                else:
-                    print("\nThe IP Address: {0} Style Error!!! ".format( tl[n] ))
-
+        print( "[*]The File: {} no Found!!!".format( pcapFile))
+    
 
 if __name__ == "__main__":
     main()
+
 
 
